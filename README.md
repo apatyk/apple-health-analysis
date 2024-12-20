2d90692892e6577b3e072c970dd88c003e39ff4f
Adam Patyk <adamamp@icloud.com>
2024-12-19 22:05:21 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # apple-health-analysis
-Simple analysis script of Apple HealthKit data
+Simple Python analysis script for Apple HealthKit data
+
+This project leverages [`uv`](https://docs.astral.sh/uv/) and the [`apple-health-parser`](https://github.com/alxdrcirilo/apple-health-parser/tree/main?tab=readme-ov-file) Python library for process Apple HealthKit data to determine useful metrics.
+
+To run the analysis:
+1. Export data from the Apple Health app.
+    1. Tap the user icon in the top right.
+    1. Scroll to the bottom.
+    1. Select "Export All Health Data"
+1. Run `uv run apple-health-analysis.py <path_to_export_zip>`
---
+++ apple-health-analysis.py
@@ -0,0 +1,50 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "apple_health_parser",
+#     "loguru",
+# ]
+# ///
+
+import argparse
+from argparse import Namespace
+
+from apple_health_parser.utils.parser import Parser
+from apple_health_parser.plot import Plot
+
+
+def parse_args() -> Namespace:
+    """Parse and return collected command-line arguments."""
+
+    parser = argparse.ArgumentParser(description="Process Apple HealthKit data")
+    parser.add_argument(
+        "path", help="Path to the export.zip archive from the Apple Health app"
+    )
+    args = parser.parse_args()
+
+    return args
+
+
+def process_data(path: str) -> None:
+    """Process and visualize Apple Health data from archive at path."""
+
+    parser = Parser(export_file=path, overwrite=False)
+
+    selected_flags = [
+        "HKQuantityTypeIdentifierAppleExerciseTime",
+        "HKQuantityTypeIdentifierDistanceWalkingRunning",
+        "HKQuantityTypeIdentifierStepCount",
+        "HKQuantityTypeIdentifierActiveEnergyBurned",
+        "HKQuantityTypeIdentifierBasalEnergyBurned",
+    ]
+    available_flags = [f for f in selected_flags if f in parser.flags]
+
+    for flag in available_flags:
+        data = parser.get_flag_records(flag=flag)
+        plt = Plot(data=data, operation="sum")
+        plt.plot(save=True)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    process_data(args.path)
